13b2f3f29faa8580ee32817715b8f4bfa5cb088b
Mustafa Khan <khannnnn0911@gmail.com>
2023-09-10 16:30:33 +0530

    All good

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Java Developer","Web Developer","React Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
